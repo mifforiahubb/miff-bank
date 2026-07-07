fc8018578d1c02eaf3f2437f7d25d60073f856ee
mifforiahubb <mifforiahubb@gmail.com>
2026-07-07 19:50:39 +0800

    Update main.py

--- Convert/main.py
+++ Convert/main.py
@@ -119,7 +119,7 @@ def convert(amount: str, rateType: str):
 
 def convertResult(robux: int, value: float, type: RobuxType):
 
-    embed = discord.Embed(title = "# $ Miffy Price Check $", 
+    embed = discord.Embed(title = "$ Miffy Price Check $", 
                           colour = discord.Colour.from_rgb(255,255,255), 
                           timestamp = datetime.now())
     embed.add_field(name = "𑣲. Order Type", value = type.value[0], inline = True)
